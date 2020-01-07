3c5fa53d08413eb4400b6649a37a0e77b724aa27
Henrik Hanssen <rhedak00@yahoo.de>
2020-01-07 11:18:31 +0900

    v0.0.9 hotfix1

--- hhpy/__version__.py
+++ hhpy/__version__.py
@@ -7,9 +7,9 @@ Information about the current version of the py-package-template package.
 
 __title__ = 'hhpy'
 __description__ = 'hhpy - convenient data science'
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 __author__ = 'Henrik Hanssen'
 __author_email__ = 'henrik.hanssen@gmail.com'
 __license__ = 'Apache 2.0'
 __url__ = 'https://github.com/rhedak/hhpy'
-__download_url__ = 'https://github.com/rhedak/hhpy/archive/v0.0.8.tar.gz'
+__download_url__ = 'https://github.com/rhedak/hhpy/archive/v0.0.9.tar.gz'
